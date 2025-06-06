7e2023784b6248c8db53cc1f492f2d0ee0fe67a9
Denys <den.lis95@hotmail.com>
2025-06-06 17:56:19 +0200

    Change URL of DB

--- common/db/product_connection.py
+++ common/db/product_connection.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-ALLURES_DB_URL = "mssql+pyodbc://localhost,1433/AlluresDb?driver=ODBC%20Driver%2017%20for%20SQL%20Server&;Trusted_Connection=yes"
+ALLURES_DB_URL = "mssql+pyodbc://sa:L#8rMw2$Dn!eX4qJ@switchyard.proxy.rlwy.net:54488/<имя_базы>?driver=ODBC+Driver+18+for+SQL+Server"
 
 engine = create_engine(ALLURES_DB_URL)
 ProductSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
