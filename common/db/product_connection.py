260db2a45477f895d33411597e423c3edbc8278c
Denys <den.lis95@hotmail.com>
2025-06-06 18:02:40 +0200

    Fixed URL of DB

--- common/db/product_connection.py
+++ common/db/product_connection.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-ALLURES_DB_URL = "mssql+pyodbc://sa:L#8rMw2$Dn!eX4qJ@switchyard.proxy.rlwy.net:54488/<имя_базы>?driver=ODBC+Driver+18+for+SQL+Server"
+ALLURES_DB_URL = os.getenv("ALLURES_DB_URL")
 
 engine = create_engine(ALLURES_DB_URL)
 ProductSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
