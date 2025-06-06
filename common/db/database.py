17ce3ef1776753311756aa1d5bfc01f9f8970ba2
Denys <den.lis95@hotmail.com>
2025-06-06 18:01:27 +0200

    Fixed URL of DB

--- common/db/database.py
+++ common/db/database.py
@@ -5,7 +5,7 @@ import os
 
 # Чтение строки подключения из .env.review
 # ALLURES_DB_URL= os.getenv("ALLURES_DB_URL", "mssql+pyodbc://sa:StrongPass123!@mssql-db:1433/AlluresDb?driver=ODBC+Driver+17+for+SQL+Server")
-ALLURES_DB_URL="mssql+pyodbc://sa:L#8rMw2$Dn!eX4qJ@switchyard.proxy.rlwy.net:54488/<имя_базы>?driver=ODBC+Driver+18+for+SQL+Server"
+ALLURES_DB_URL = os.getenv("ALLURES_DB_URL")
 
 engine = create_engine(ALLURES_DB_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
