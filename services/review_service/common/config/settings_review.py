725105fc74e865bd8d74c6cfc56540b64023e64e
Denys <den.lis95@hotmail.com>
2025-06-06 21:43:09 +0200

    Change LOCAL_DB to ALLURES_DB

--- services/review_service/common/config/settings_review.py
+++ services/review_service/common/config/settings_review.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 from pathlib import Path
 
 class ReviewSettings(BaseSettings):
-    LOCAL_DB_URL: str
+    ALLURES_DB_URL: str #Change to LOCAL_DB_URL after
 
     class Config:
         env_file = Path(__file__).resolve().parents[4] / ".env.review"
