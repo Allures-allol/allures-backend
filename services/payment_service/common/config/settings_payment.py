ecd80fe7f7e662e1b82debdc012ccbe5125c70ad
Pavlo-fo95 <Pavlo-fo95@student.itstep.org>
2025-06-16 16:26:54 +0300

    Fix payment_service settings: allow extra env vars

--- services/payment_service/common/config/settings_payment.py
+++ services/payment_service/common/config/settings_payment.py
@@ -2,13 +2,21 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    NOWPAYMENTS_API_KEY: str
-    NGROK_WEBHOOK_URL: str = ""
     MAINDB_URL: str
+    SA_PASSWORD: str
+    PRODUCT_SERVICE_URL: str
+    SALES_SERVICE_URL: str
+    REVIEW_SERVICE_URL: str
+    AUTH_SERVICE_URL: str
+    PROFILE_SERVICE_URL: str
+    PAYMENTS_SERVICE_URL: str
+    DISCOUNT_SERVICE_URL: str
+    DASHBOARD_SERVICE_URL: str
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "allow"  # ✅ если хочешь не указывать всё вручную
 
 settings_payment = Settings()
 
