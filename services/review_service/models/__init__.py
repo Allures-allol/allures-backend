5ed36fcb711198a3b316dba58fb97ae42712ac51
Pavlo-fo95 <Pavlo-fo95@student.itstep.org>
2025-08-17 06:08:15 +0300

    cleanup: keep only auth_service + common

--- services/review_service/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# services/review_service/models/__init__.py
-# from .review import Review
-# from .recommendation import Recommendation
--- services/sales_service/api/schemas/inventory.py
+++
@@ -1,9 +0,0 @@
-# api/schemas/inventory.py
-from pydantic import BaseModel
-from common.enums.product_enums import ProductCategory
-
-class InventoryCreate(BaseModel):
-    product_id: int
-    category_id: int
-    inventory_quantity: int
-
